66134f2ea4e760196e454c99beadcea1e3952bc1
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-28 17:55:33 -0300

    add aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+app.use(express.json());
+app.use(routes);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log("Server is Running on port " + PORT));
